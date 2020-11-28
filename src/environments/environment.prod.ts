7a686cf7cc58ea95c6e1a685431aebbeab35d3fa
penbosso <ckbosso@st.ug.edu.gh>
2020-11-28 08:35:59 +0000

    feat: angular, firebase, and material ui set up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD5jXpHAfqGn9wnuTLrWb7QWaONxVLO5R0",
+    authDomain: "tim-calendar-fbe2b.firebaseapp.com",
+    databaseURL: "https://tim-calendar-fbe2b.firebaseio.com",
+    projectId: "tim-calendar-fbe2b",
+    storageBucket: "tim-calendar-fbe2b.appspot.com",
+    messagingSenderId: "781691037932",
+    appId: "1:781691037932:web:1f106b4e19904b5ee51dc9"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyD5jXpHAfqGn9wnuTLrWb7QWaONxVLO5R0",
+    authDomain: "tim-calendar-fbe2b.firebaseapp.com",
+    databaseURL: "https://tim-calendar-fbe2b.firebaseio.com",
+    projectId: "tim-calendar-fbe2b",
+    storageBucket: "tim-calendar-fbe2b.appspot.com",
+    messagingSenderId: "781691037932",
+    appId: "1:781691037932:web:1f106b4e19904b5ee51dc9"
+  }
 };
 
 /*
